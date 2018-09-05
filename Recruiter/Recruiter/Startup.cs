9ef77ff4858e2eb7d2dca197f1ea991caafb002e
bpelikan <bartoszpelikan@gmail.com>
2018-09-05 23:24:25 +0200

    Globalization and localization

--- Recruiter/Recruiter/Startup.cs
+++ Recruiter/Recruiter/Startup.cs
@@ -11,6 +11,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Recruiter.Data;
 using Recruiter.Models;
 using Recruiter.Services;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
 
 namespace Recruiter
 {
@@ -36,7 +40,26 @@ namespace Recruiter
             // Add application services.
             services.AddTransient<IEmailSender, EmailSender>();
 
-            services.AddMvc();
+            services.AddLocalization(opts => { opts.ResourcesPath = "Resources"; });
+            services
+                .AddMvc()
+                .AddViewLocalization(
+                    LanguageViewLocationExpanderFormat.Suffix,
+                    opts => { opts.ResourcesPath = "Resources"; })
+                .AddDataAnnotationsLocalization();
+            services.Configure<RequestLocalizationOptions>(
+                opts =>
+                {
+                    var supportedCultures = new List<CultureInfo>
+                    {
+                        new CultureInfo("en-US"),
+                        new CultureInfo("pl-PL"),
+                    };
+
+                    opts.DefaultRequestCulture = new RequestCulture("pl-PL");
+                    opts.SupportedCultures = supportedCultures;
+                    opts.SupportedUICultures = supportedCultures;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +77,8 @@ namespace Recruiter
             }
 
             app.UseStaticFiles();
+
+            app.UseRequestLocalization(app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>().Value);
 
             app.UseAuthentication();
 
