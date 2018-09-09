75ea7e2d6bd53c9a0a840ec88a0203522fcc39cf
bpelikan <bartoszpelikan@gmail.com>
2018-09-09 18:14:36 +0200

    RegisterViewModel

--- Recruiter/Recruiter/Models/AccountViewModels/RegisterViewModel.cs
+++ Recruiter/Recruiter/Models/AccountViewModels/RegisterViewModel.cs
@@ -8,23 +8,25 @@ namespace Recruiter.Models.AccountViewModels
 {
     public class RegisterViewModel
     {
+        [Required(ErrorMessage = "The {0} field is required.")]
         [Display(Name = "First name")]
-        [Required(ErrorMessage = "{0} is required")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "The {0} field is required.")]
         [Display(Name = "Last name")]
-        [Required(ErrorMessage = "{0} is required")]
         public string LastName { get; set; }
 
+        [Required(ErrorMessage = "The {0} field is required.")]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
 
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "The {0} field is required.")]
+        [EmailAddress(ErrorMessage = "The {0} field is not a valid e-mail address.")]
+        //[DataType(DataType.EmailAddress)]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "The {0} field is required.")]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
