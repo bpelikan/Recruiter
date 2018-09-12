37bc630e8d671c729f1032ec9b971b0054c207ff
bpelikan <bartoszpelikan@gmail.com>
2018-09-12 23:29:38 +0200

    Cv model

---
+++ Recruiter/Recruiter/Models/Cv.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recruiter.Models
+{
+    public class Cv
+    {
+        public string Id { get; set; }
+
+        public string FileName { get; set; }
+
+        //public string UserId { get; set; }
+        //public virtual ApplicationUser User { get; set; }
+    }
+}
