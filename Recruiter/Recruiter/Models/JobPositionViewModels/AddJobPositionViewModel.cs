58ce318e75ad924fcbae971a1d109b0965199e87
bpelikan <bartoszpelikan@gmail.com>
2018-09-12 23:30:38 +0200

    AddJobPositionViewModel

---
+++ Recruiter/Recruiter/Models/JobPositionViewModels/AddJobPositionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recruiter.Models.JobPositionViewModels
+{
+    public class AddJobPositionViewModel
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
