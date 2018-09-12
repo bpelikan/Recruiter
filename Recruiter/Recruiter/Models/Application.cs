2bfb47ca1eaf83aeef69905a25e13341b0443a61
bpelikan <bartoszpelikan@gmail.com>
2018-09-12 23:28:02 +0200

    Application model

---
+++ Recruiter/Recruiter/Models/Application.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recruiter.Models
+{
+    public class Application
+    {
+        public string Id { get; set; }
+
+        public string CvId { get; set; }
+        public virtual Cv Cv { get; set; }
+
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        public string JobPositionId { get; set; }
+        public virtual JobPosition JobPosition { get; set; }
+
+    }
+}
