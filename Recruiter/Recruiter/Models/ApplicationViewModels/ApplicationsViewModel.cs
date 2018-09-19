43477f8eb7a28096369e7ca8efe83d61212ff1d6
bpelikan <bartoszpelikan@gmail.com>
2018-09-19 22:28:58 +0200

    Applications view

---
+++ Recruiter/Recruiter/Models/ApplicationViewModels/ApplicationsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recruiter.Models.ApplicationViewModels
+{
+    public class ApplicationsViewModel
+    {
+        public string Id { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm:ss}")]
+        public DateTime CreatedAt { get; set; }
+
+        public virtual JobPositionViewModel JobPosition { get; set; }
+
+        public virtual UserDetailsViewModel User { get; set; }
+    }
+}
