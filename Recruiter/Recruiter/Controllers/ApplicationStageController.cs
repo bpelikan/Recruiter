b81a16919f67ecf9214ee708a7092381faf9f3df
bpelikan <bartoszpelikan@gmail.com>
2018-10-30 19:42:03 +0100

    Add ApplicationStageController with AssingUserToApplicationStage method

---
+++ Recruiter/Recruiter/Controllers/ApplicationStageController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Recruiter.Data;
+using Recruiter.Models;
+using Recruiter.Models.ApplicationStageViewModels;
+using Recruiter.Shared;
+
+namespace Recruiter.Controllers
+{
+    public class ApplicationStageController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ApplicationStageController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [Authorize(Roles = RoleCollection.Administrator + "," + RoleCollection.Recruiter)]
+        public async Task<IActionResult> AssingUserToApplicationStage(string stageId)
+        {
+            var stage = await _context.ApplicationStages.Include(x => x.Application).FirstOrDefaultAsync(x => x.Id == stageId);
+
+            if (stage != null)
+            {
+                var vm = new AssingUserToStageViewModel()
+                {
+                    ApplicationId = stage.ApplicationId,
+                    StageId = stage.Id,
+                };
+
+                var users = await _userManager.GetUsersInRoleAsync(RoleCollection.Recruiter);
+                if(users.Count() != 0)
+                    ViewData["UsersToAssingToStage"] = new SelectList(users, "Id", "Email");
+                return View(vm);
+            }
+
+            throw new Exception($"ApplicationStage with id {stageId} not found. (UserID: {_userManager.GetUserId(HttpContext.User)})");
+        }
+
+        [HttpPost]
+        [Authorize(Roles = RoleCollection.Administrator + "," + RoleCollection.Recruiter)]
+        public async Task<IActionResult> AssingUserToApplicationStage(AssingUserToStageViewModel addResponsibleUserToStageViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(addResponsibleUserToStageViewModel);
+
+            var stage = await _context.ApplicationStages.FirstOrDefaultAsync(x => x.Id == addResponsibleUserToStageViewModel.StageId);
+
+            if (stage != null)
+            {
+                stage.ResponsibleUserId = addResponsibleUserToStageViewModel.UserId;
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(ApplicationController.ApplicationDetails), "Application", new { id = addResponsibleUserToStageViewModel.ApplicationId });
+            }
+
+            throw new Exception($"ApplicationStage with id {addResponsibleUserToStageViewModel.StageId} not found. (UserID: {_userManager.GetUserId(HttpContext.User)})");
+        }
+    }
+}
---
+++ Recruiter/Recruiter/Models/ApplicationStageViewModels/AssingUserToStageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recruiter.Models.ApplicationStageViewModels
+{
+    public class AssingUserToStageViewModel
+    {
+        public string ApplicationId { get; set; }
+
+        public string StageId { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+    }
+}
