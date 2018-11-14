682462b14aab2eb530b6cfe696e2beabdfab4a15
bpelikan <bartoszpelikan@gmail.com>
2018-11-14 18:44:49 +0100

    Enable nullable Rate value in ApplicationStageBase

---
+++ Recruiter/Recruiter/Data/Migrations/20181114173831_EnableNullableRateValueInApplicationStageBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Recruiter.Data.Migrations
+{
+    public partial class EnableNullableRateValueInApplicationStageBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Rate",
+                table: "ApplicationStages",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Rate",
+                table: "ApplicationStages",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+        }
+    }
+}
--- Recruiter/Recruiter/Models/ApplicationStageBase.cs
+++ Recruiter/Recruiter/Models/ApplicationStageBase.cs
@@ -20,7 +20,7 @@ namespace Recruiter.Models
         public ApplicationStageState State { get; set; }
         public bool Accepted { get; set; }
         public string Note { get; set; }
-        public int Rate { get; set; }
+        public int? Rate { get; set; }
 
         public string ApplicationId { get; set; }
         public virtual Application Application { get; set; }
