d889b9a3099057ea3cfcf0efed413ec31373f4d1
bpelikan <bartoszpelikan@gmail.com>
2018-12-01 23:48:53 +0100

    Add new custom errors

---
+++ Recruiter/Recruiter/CustomExceptions/BlobOperationException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recruiter.CustomExceptions
+{
+    public class BlobOperationException : CustomException
+    {
+        public BlobOperationException()
+        {
+        }
+        public BlobOperationException(string message) : base(message)
+        {
+        }
+        public BlobOperationException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
---
+++ Recruiter/Recruiter/CustomExceptions/InvalidFileExtensionException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recruiter.CustomExceptions
+{
+    public class InvalidFileExtensionException : CustomException
+    {
+        public InvalidFileExtensionException()
+        {
+        }
+        public InvalidFileExtensionException(string message) : base(message)
+        {
+        }
+        public InvalidFileExtensionException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
