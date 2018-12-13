154dda62a85860f8bb58c03c4dece611b787ec0e
bpelikan <bartoszpelikan@gmail.com>
2018-12-13 23:37:05 +0100

    .

--- Recruiter/Recruiter/Extensions/EmailSenderExtensions.cs
+++ Recruiter/Recruiter/Extensions/EmailSenderExtensions.cs
@@ -94,13 +94,13 @@ namespace Recruiter.Services
             <table border='0' cellpadding='0' cellspacing='0' width='100%'>
 	            <tr>
 		            <td style='color: #153643; font-family: Arial, sans-serif; font-size: 24px;'>
-			            <b>--TYTUL orem ipsum dolor sit amet, consectetur adipiscing elit.</b>
+			            <b>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b>
                         
 		            </td>
 	            </tr>
 	            <tr>
 		            <td style='padding: 20px 0 30px 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;'>
-                        ----TRESC EMAILA One of the stages of your application has changed the state, 
+                        One of the stages of your application has changed the state, 
 			            check by clicking this link: <a href='{HtmlEncoder.Default.Encode(link)}'>link</a>
                         <br/>
 			            tiam sit amet bibendum turpis. Praesent a ornare leo. Aliquam id venenatis nisl, eu venenatis est. 
