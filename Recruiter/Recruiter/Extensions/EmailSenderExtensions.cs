5c04c8dda6908753b87161e7e170c503a52546c1
bpelikan <bartoszpelikan@gmail.com>
2018-12-14 21:08:32 +0100

    .

--- Recruiter/Recruiter/Extensions/EmailSenderExtensions.cs
+++ Recruiter/Recruiter/Extensions/EmailSenderExtensions.cs
@@ -12,8 +12,16 @@ namespace Recruiter.Services
     {
         public static Task SendEmailConfirmationAsync(this IEmailSender emailSender, string email, string link)
         {
-            return emailSender.SendEmailAsync(email, "Confirm your email",
-                $"Please confirm your account by clicking this link: <a href='{HtmlEncoder.Default.Encode(link)}'>link</a>");
+            string subject = $"Confirm your email";
+            string title = $"Confirm your email";
+            string content = $@"
+                                Please confirm your account by clicking this link: <a href='{HtmlEncoder.Default.Encode(link)}'>link</a>
+                                ";
+
+            return emailSender.SendEmailAsync(email, subject, EmailTemplate(title, content));
+
+            //return emailSender.SendEmailAsync(email, "Confirm your email",
+            //    $"Please confirm your account by clicking this link: <a href='{HtmlEncoder.Default.Encode(link)}'>link</a>");
         }
 
         public static Task SendEmailNotificationProcessApplicationApprovalAsync(this IEmailSender emailSender, string email, string link, ApplicationStageBase stage)
@@ -32,7 +40,7 @@ namespace Recruiter.Services
 
             return emailSender.SendEmailAsync(email, subject, EmailTemplate(title,content));
                 
-            //$@"
+                //$@"
                 //    <!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
                 //    <html xmlns='http://www.w3.org/1999/xhtml'>
                 //    <head>
@@ -146,68 +154,68 @@ namespace Recruiter.Services
         private static string EmailTemplate(string title, string content)
         {
             return $@"
-<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
-<html xmlns='http://www.w3.org/1999/xhtml'>
-<head>
-	<meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
-	<title>Demystifying Email Design</title>
-	<meta name='viewport' content='width=device-width, initial-scale=1.0'/>
-</head>                
-<body style='margin: 0; padding: 0;'>
-<table align='center' border='0' cellpadding='0' cellspacing='0' width='600' style='border-collapse: collapse; border: 1px solid #cccccc;'>
-	<tr>
-		<td align='center' bgcolor='#0254E6' style='padding: 0 0 0 0; '>
-			<img src='https://recruiterbpstorage.blob.core.windows.net/static/career-3449422_640.png' alt='ASP.NET' width='600' height='300' style='display: block;' />
-		</td>
-	</tr>
-	<tr>
-		<td bgcolor='#ffffff' style='padding: 40px 30px 40px 30px;'>
+                    <!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+                    <html xmlns='http://www.w3.org/1999/xhtml'>
+                    <head>
+	                    <meta http-equiv='Content-Type' content='text/html; charset=UTF-8' />
+	                    <title>Demystifying Email Design</title>
+	                    <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
+                    </head>                
+                    <body style='margin: 0; padding: 0;'>
+                    <table align='center' border='0' cellpadding='0' cellspacing='0' width='600' style='border-collapse: collapse; border: 1px solid #cccccc;'>
+	                    <tr>
+		                    <td align='center' bgcolor='#0254E6' style='padding: 0 0 0 0; '>
+			                    <img src='https://recruiterbpstorage.blob.core.windows.net/static/career-3449422_640.png' alt='ASP.NET' width='600' height='300' style='display: block;' />
+		                    </td>
+	                    </tr>
+	                    <tr>
+		                    <td bgcolor='#ffffff' style='padding: 40px 30px 40px 30px;'>
 
-            <table border='0' cellpadding='0' cellspacing='0' width='100%'>
-	            <tr>
-		            <td style='color: #153643; font-family: Arial, sans-serif; font-size: 24px;'>
-			            <b>
-                            {title}
-                        </b>
-		            </td>
-	            </tr>
-	            <tr>
-		            <td style='padding: 20px 0 30px 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;'>
-                        {content}
-		            </td>
-	            </tr>
-            </table>
-		</td>
+                                <table border='0' cellpadding='0' cellspacing='0' width='100%'>
+	                                <tr>
+		                                <td style='color: #153643; font-family: Arial, sans-serif; font-size: 24px;'>
+			                                <b>
+                                                {title}
+                                            </b>
+		                                </td>
+	                                </tr>
+	                                <tr>
+		                                <td style='padding: 20px 0 30px 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;'>
+                                            {content}
+		                                </td>
+	                                </tr>
+                                </table>
+		                    </td>
         
-	</tr>
-	<tr>
-		<td bgcolor='#ee4c50' style='padding: 30px 30px 30px 30px;'>
-			<table border='0' cellpadding='0' cellspacing='0' width='100%'>
-				<tr>
-					<td width='75%' style='color: #ffffff; font-family: Arial, sans-serif; font-size: 14px;'>
-						&reg; <a href='https://recruiterbp.azurewebsites.net/' style='color: #ffffff;'>Recruiter {DateTime.UtcNow.Year}</a>  
+	                    </tr>
+	                    <tr>
+		                    <td bgcolor='#ee4c50' style='padding: 30px 30px 30px 30px;'>
+			                    <table border='0' cellpadding='0' cellspacing='0' width='100%'>
+				                    <tr>
+					                    <td width='75%' style='color: #ffffff; font-family: Arial, sans-serif; font-size: 14px;'>
+						                    &reg; <a href='https://recruiterbp.azurewebsites.net/' style='color: #ffffff;'>Recruiter {DateTime.UtcNow.Year}</a>  
 						
-						<br/>
-					</td>
-					<td align='right'>
-						<table border='0' cellpadding='0' cellspacing='0'>
-							<tr>
-								<td>
-									<a href='https://recruiterbp.azurewebsites.net/'>
-										<img src='https://recruiterbpstorage.blob.core.windows.net/static/website.png' alt='Facebook' width='38' height='38' style='display: block;' border='0' />
-									</a>
-								</td>
-							</tr>
-						</table>
-					</td>
-				</tr>
-			</table>
-		</td>
-	</tr>
-</table>
-</body>
-</html>
-";
+						                    <br/>
+					                    </td>
+					                    <td align='right'>
+						                    <table border='0' cellpadding='0' cellspacing='0'>
+							                    <tr>
+								                    <td>
+									                    <a href='https://recruiterbp.azurewebsites.net/'>
+										                    <img src='https://recruiterbpstorage.blob.core.windows.net/static/website.png' alt='Facebook' width='38' height='38' style='display: block;' border='0' />
+									                    </a>
+								                    </td>
+							                    </tr>
+						                    </table>
+					                    </td>
+				                    </tr>
+			                    </table>
+		                    </td>
+	                    </tr>
+                    </table>
+                    </body>
+                    </html>
+                    ";
         }
     }
 }
