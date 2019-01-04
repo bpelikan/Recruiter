4496908102915e1e5c7a1c7850d780d22b12da13
bpelikan <bartoszpelikan@gmail.com>
2019-01-04 17:56:42 +0100

    Update ProcessHomeworkStage view

--- Recruiter/Recruiter/Models/ApplicationStageViewModels/ProcessHomeworkStageViewModel.cs
+++ Recruiter/Recruiter/Models/ApplicationStageViewModels/ProcessHomeworkStageViewModel.cs
@@ -18,21 +18,29 @@ namespace Recruiter.Models.ApplicationStageViewModels
     public class HomeworkViewModel
     {
         public string Id { get; set; }
+        [Display(Name = "Note")]
         public string Note { get; set; }
 
+        [Display(Name = "Rate")]
         [Range(1, 5, ErrorMessage = "Rate must be beetween 1 and 5")]
         public int Rate { get; set; }
+        [Display(Name = "Description")]
         public string Description { get; set; }
+        [Display(Name = "Duration")]
         public int Duration { get; set; }
 
+        [Display(Name = "Start Time")]
         [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy HH:mm:ss}")]
         public DateTime? StartTime { get; set; }
 
+        [Display(Name = "End Time")]
         [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy HH:mm:ss}")]
         public DateTime? EndTime { get; set; }
 
+        [Display(Name = "Url")]
         public string Url { get; set; }
 
+        [Display(Name = "Sending Time")]
         [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy HH:mm:ss}")]
         public DateTime? SendingTime { get; set; }
     }
